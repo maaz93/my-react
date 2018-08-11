4973a28610ca530d3b5ad83c37d64559645e8c13
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-08-11 21:27:00 +0530

    Fix bug. Support text nodes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
+export const ELEMENT_TYPES = {
+  TEXT_ELEMENT: 'TEXT_ELEMENT'
+};
+
 export function render(element, parentDom) {
   const {
     type,
     props: { children, ...otherProps }
   } = element;
-  const dom = document.createElement(type);
+
+  // Create DOM element
+  const dom =
+    type === ELEMENT_TYPES.TEXT_ELEMENT
+      ? document.createTextNode('')
+      : document.createElement(type);
   const { events, attributes } = splitEventsAndAttributes(otherProps);
 
+  // Add events
   Object.keys(events).forEach(eventName =>
     dom.addEventListener(eventName, events[eventName])
   );
 
+  // Add attributes
   Object.keys(attributes).forEach(
     attributeName => (dom[attributeName] = attributes[attributeName])
   );
 
+  // Render children
   const childElements = children || [];
   childElements.forEach(childElement => render(childElement, dom));
   parentDom.appendChild(dom);
 }
 
-const splitEventsAndAttributes = props => {
+const splitEventsAndAttributes = (props = {}) => {
   return Object.keys(props).reduce(
     (memo, prop) => {
       if (prop.indexOf('on') === 0) {
         memo.events[prop] = props[prop];
       } else {
-        memo.attributes[props] = props[prop];
+        memo.attributes[prop] = props[prop];
       }
+      return memo;
     },
     {
       events: {},
