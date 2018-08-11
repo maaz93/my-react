caa3d233c036007c1ed25e4e6185ad483677122b
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-08-11 22:26:13 +0530

    Reduce dependencies. Lint custom pragma. Fix linting pragma

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/react'],
+            presets: ['@babel/preset-env'],
             plugins: [
               [
                 '@babel/transform-react-jsx',
