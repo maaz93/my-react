0466662cd771c506dba84258c060984328683e1e
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-08-11 19:21:54 +0530

    Setup dev server

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,10 @@
+import { test } from '../../src';
+
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = ['Hello', 'webpack'].join(' ');
+  element.innerHTML = ['Hello', 'webpack', test].join(' ');
 
   return element;
 }
