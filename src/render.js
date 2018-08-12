792ec33a5c2a6ae6de4f4cf14d91c8bd593fa71e
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-08-12 18:48:01 +0530

    Change folder structure

---
+++ src/render.js
@@ -0,0 +1,103 @@
+import { ELEMENT_TYPES } from './elementTypes';
+
+let rootInstance = null;
+
+export function render(element, container) {
+  const prevInstance = rootInstance;
+  const nextInstance = reconcile(container, prevInstance, element);
+  rootInstance = nextInstance;
+}
+
+function reconcile(parentDom, instance, element) {
+  if (instance == null) {
+    // Create instance
+    const newInstance = instantiate(element);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance;
+  } else if (element == null) {
+    parentDom.removeChild(instance.dom);
+    return null;
+  } else if (instance.element.type === element.type) {
+    // Update instance
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
+    instance.element = element;
+    return instance;
+  } else {
+    // Replace instance
+    const newInstance = instantiate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance;
+  }
+}
+
+function reconcileChildren(instance, element) {
+  const { dom, childInstances } = instance;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter(newChildInstance => newChildInstance != null);
+}
+
+function instantiate(element) {
+  const { type, props } = element;
+
+  // Create DOM element
+  const isTextElement = type === ELEMENT_TYPES.TEXT_ELEMENT;
+  const dom = isTextElement
+    ? document.createTextNode('')
+    : document.createElement(type);
+
+  updateDomProperties(dom, [], props);
+
+  // Instantiate and append children
+  const childElements = props.children || [];
+  const childInstances = childElements.map(instantiate);
+  const childDoms = childInstances.map(childInstance => childInstance.dom);
+  childDoms.forEach(childDom => dom.appendChild(childDom));
+
+  const instance = { dom, element, childInstances };
+  return instance;
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = name => name.startsWith('on');
+  const isAttribute = name => !isEvent(name) && name != 'children';
+
+  // Remove event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove attributes
+  Object.keys(prevProps)
+    .filter(isAttribute)
+    .forEach(name => {
+      dom[name] = null;
+    });
+
+  // Set attributes
+  Object.keys(nextProps)
+    .filter(isAttribute)
+    .forEach(name => {
+      dom[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
